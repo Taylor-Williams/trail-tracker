d7f39ba13e056d8bf67e4c38e1986335e65c04f0
Taylor Williams <taylor@thewilliams.net>
2018-09-07 00:10:31 +0000

    add login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    erb :login
+    if !logged_in?
+      erb :login
+    else
+      redirect "/users/#{current_user.slug}"
+    end
   end
 
   get '/signup' do
@@ -20,13 +24,32 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/logout' do
+    session.delete(:user_id)
     redirect '/'
   end
 
   post '/signup' do
-    @user = User.create(username: params[:username], password: params[:password], email: params[:email])
-    @user.bio = params[:bio] if !params[:bio].empty?
-    redirect "/users/#{@user.id}"
+    @user = User.create(username: params[:username], password: params[:password], email: params[:email], bio: params[:bio])
+    if !@user.valid?
+      @errors = @user.errors.messages
+      erb :errors
+    else
+      redirect "/users/#{@user.slug}"
+    end
+  end
+
+  post '/login' do
+    if !params[:username].empty? && !params[:password].empty?
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/users/#{@user.slug}"
+      else
+        redirect '/login'
+      end
+    else
+      redirect '/login'
+    end
   end
 
   helpers do
@@ -34,7 +57,7 @@ class ApplicationController < Sinatra::Base
       !!session[:user_id]
     end
 
-    def curret_user
+    def current_user
       session[:user_id] && User.find(session[:user_id])
     end
   end
