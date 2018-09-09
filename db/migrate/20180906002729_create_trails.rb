3dbc42453aa5875aa6046be1c00a7872c979ae24
Taylor Williams <taylor@thewilliams.net>
2018-09-09 05:52:33 +0000

    make trails migration work

--- db/migrate/20180906002729_create_trails.rb
+++ db/migrate/20180906002729_create_trails.rb
@@ -1,12 +1,12 @@
 class CreateTrails < ActiveRecord::Migration
   def change
     create_table :trails do |t|
-      t.real :length
+      t.float :length
       t.string :name
       t.string :slug
       t.integer :difficulty
-      t.real :start_alt
-      t.real :end_alt
+      t.float :start_alt
+      t.float :end_alt
       t.integer :state_id
     end
   end
