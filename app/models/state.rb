e87c2cd1a703004a1e02d0d9f45023c275bee8c4
Taylor Williams <taylor@thewilliams.net>
2018-09-11 14:28:36 +0000

    make state validations

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,4 +1,7 @@
 class State < ActiveRecord::Base
   has_many :trails
   has_many :users, through: :trails
+
+  validates :name, presence: true
+  validates :code, presence: true, length: { is: 2 }
 end
