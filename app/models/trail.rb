0a1da9edd74d3a029abf074663be2653d379a9b8
Taylor Williams <taylor@thewilliams.net>
2018-09-11 14:15:37 +0000

    fix trial, user validations

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -4,16 +4,20 @@ class Trail < ActiveRecord::Base
   has_many :users, through: :user_trails
 
   validates :name, presence: true, uniqueness: true
-  validates :length, numericality: true
-  validates :difficulty, numericality: { only_integer: true, greater_than_or_eqaul_to: 0, less_than_or_eqaul_to: 10}
-  validates :start_alt, numericality: true
-  validates :end_alt, numericality: true
+  validates :length, numericality: true, allow_nil: true
+  validates :difficulty, numericality: { only_integer: true, greater_than_or_eqaul_to: 0, less_than_or_eqaul_to: 10}, allow_nil: true
+  validates :start_alt, numericality: true, allow_nil: true
+  validates :end_alt, numericality: true, allow_nil: true
+
+  validate :both_alts_present
+
+  def both_alts_present
+    errors.add(:alts, "must include both a starting and ending altitude") if end_alt && !start_alt || start_alt && !end_alt
+  end
 
   before_save do
-    self.slug = self.username.gsub(/[^a-zA-Z0-9]+/, "-").downcase
-    self.slug.slice![-2] if self.slug =~ /-\z/
-    validates :slug, presence: true, uniqueness: true
-    validates :end_alt, presence: true if self.start_alt?
-    validates :start_alt, presence: true if self.end_alt?
+    self.slug = self.name.gsub(/[^a-zA-Z0-9]+/, "-").downcase
+    self.slug.slice!(-1) if self.slug =~ /-\z/
+    #validates :slug, presence: true, uniqueness: true
   end
 end
