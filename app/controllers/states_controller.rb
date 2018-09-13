df720f40814673aab1020ddf53084044a51c428f
Taylor Williams <taylor@thewilliams.net>
2018-09-13 22:56:09 +0000

    add state index and show functions

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,2 +1,16 @@
 class StatesController < ApplicationController
+
+  get '/states' do
+    @states = State.all
+    erb :'states/index'
+  end
+
+  get '/states/:code' do
+    @state = State.find_by(code: params[:code])
+    if @state
+      erb :"states/show"
+    else
+      redirect '/states'
+    end
+  end
 end
