2f0015a292d7b888740ac1e8ef88db6d329fd198
Taylor Williams <taylor@thewilliams.net>
2018-09-19 00:36:24 +0000

    add ability to edit trail on show page

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -6,8 +6,8 @@ class TrailsController < ApplicationController
   end
 
   post '/trails' do
-    @trail = Trail.new(name: params[:name])
-    trail_attrs = %i(length difficulty start_alt end_alt)
+    @trail = Trail.new
+    trail_attrs = %i(name length difficulty start_alt end_alt)
     trail_attrs.each do |attribute|
       if !params[attribute].empty?
         @trail.send("#{attribute}=", params[attribute])
@@ -37,6 +37,29 @@ class TrailsController < ApplicationController
       redirect '/trails'
     end
   end
+
+  post '/trails/:slug/edit' do
+    @trail = Trail.find_by(slug: params[:slug])
+    trail_attrs = %i(name length difficulty start_alt end_alt)
+    trail_attrs.each do |attribute|
+      if !params[attribute].empty?
+        @trail.send("#{attribute}=", params[attribute])
+      end
+    end
+    @trail.save
+    if !@trail.valid?
+      @errors = @trail.errors.messages
+      erb :errors
+    else
+      if params[:user_id] && !@trail.user_ids.include?(:user_id)
+        @trail.user_trails.create(user_id: params[:user_id])
+      elsif !params[:user_id] && @trail.user_ids.include?(current_user.id)
+        UserTrail.find_by(trail_id: @trail.id, user_id: current_user.id).destroy
+      end
+      redirect "/trails/#{@trail.slug}"
+    end
+  end
+
 
 
 end
