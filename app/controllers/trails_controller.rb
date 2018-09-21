37e85df9aafbe497baf8be35015569841ff6f12a
Taylor Williams <taylor@thewilliams.net>
2018-09-21 00:01:31 +0000

    add creator validation to trail edit/destroy functions in controller

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -65,39 +65,44 @@ class TrailsController < ApplicationController
 
   post '/trails/:slug/edit' do
     @trail = Trail.find_by(slug: params[:slug])
-    trail_attrs = %i(name length difficulty start_alt end_alt)
-    trail_attrs.each do |attribute|
-      if !params[attribute].empty?
-        @trail.send("#{attribute}=", params[attribute])
+    if @trail.creator_id == current_user.id
+      trail_attrs = %i(name length difficulty start_alt end_alt)
+      trail_attrs.each do |attribute|
+        if !params[attribute].empty?
+          @trail.send("#{attribute}=", params[attribute])
+        end
       end
-    end
-    if !params[:state].empty?
-        if params[:state].length == 2
-          state = State.where('lower(code) = ?', params[:state].downcase).first
-        else
-          state = State.where('lower(name) = ?', params[:state].downcase).first
+      if !params[:state].empty?
+          if params[:state].length == 2
+            state = State.where('lower(code) = ?', params[:state].downcase).first
+          else
+            state = State.where('lower(name) = ?', params[:state].downcase).first
+          end
+          if state
+            @trail.state = state
+          end
+      end
+      @trail.save
+      if !@trail.valid?
+        flash[:error] = @trail.errors.messages.map do |k,v|
+          "<p>there was an issue with your #{k}: #{v.join(", ")}</p>"
+        end.join
+      else
+        if params[:user_id] && !@trail.user_ids.include?(:user_id)
+          @trail.user_trails.create(user_id: params[:user_id])
+        elsif !params[:user_id] && @trail.user_ids.include?(current_user.id)
+          UserTrail.find_by(trail_id: @trail.id, user_id: current_user.id).destroy
         end
-        if state
-          @trail.state = state
-        end
-    end
-    @trail.save
-    if !@trail.valid?
-      flash[:error] = @trail.errors.messages.map do |k,v|
-        "<p>there was an issue with your #{k}: #{v.join(", ")}</p>"
-      end.join
-    else
-      if params[:user_id] && !@trail.user_ids.include?(:user_id)
-        @trail.user_trails.create(user_id: params[:user_id])
-      elsif !params[:user_id] && @trail.user_ids.include?(current_user.id)
-        UserTrail.find_by(trail_id: @trail.id, user_id: current_user.id).destroy
       end
     end
     redirect "/trails/#{@trail.slug}"
   end
 
   post '/trails/:slug/destroy' do
-    Trail.find_by(slug: params[:slug]).destroy
+    @trail = Trail.find_by(slug: params[:slug])
+    if @trail.creator_id == current_user.id
+      @trail.destroy
+    end
     redirect '/trails'
   end
 end
